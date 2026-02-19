4ca187647c3b27fcd7e65cdb89602206de7cdd5b
David GAILLETON <david.gailleton@posteo.net>
2026-02-19 11:20:59 +0100

    01/01 WIP

--- 07/ex1/Deck.py
+++ 07/ex1/Deck.py
@@ -1,6 +1,25 @@
 from ex0.Card import Card
 
 
-class Deck():
+class Deck:
     def __init__(self) -> None:
-        self: 
+        self.cards: list[Card] = []
+
+    def add_card(self, card: Card) -> None:
+        self.cards += [card]
+
+    def remove_card(self, card_name: Card) -> None:
+        for n in range(len(self.cards)):
+            if self.cards[n].name == card_name:
+                self.cards.pop(n)
+                return
+        print("{card_name} not found")
+
+    def shuffle(self) -> None:
+        pass
+
+    def draw_card(self) -> Card:
+        pass
+
+    def get_deck_stats(self) -> dict:
+        pass
